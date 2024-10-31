b618be2fd4adc9521d7a30ac4761d9ed5f571c8e
franksymon <frank.urbina.dev@gmail.com>
2024-10-31 07:51:39 -0400

    [FIX] se bug de dependecia circular

--- app/user/model.py
+++ app/user/model.py
@@ -1,10 +1,14 @@
 from app.core.db import BaseModel
 from pydantic import EmailStr
 from sqlmodel import  Field, Column, String, Boolean, Relationship, func, DateTime
-from app.role.model import RoleEntity
 from datetime import datetime
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 
+# Models
+if TYPE_CHECKING:
+    from app.role.model import RoleEntity
+    from app.operation.model import OperationEntity
+    from app.bid.model import BidEntity
 
 class UserEntity(BaseModel, table=True):
     __tablename__ = "users"
@@ -15,14 +19,16 @@ class UserEntity(BaseModel, table=True):
     is_active: bool = Field(default=True, sa_column=Column(Boolean, nullable=False))
     hashed_password: str = Field(sa_column=Column(String, nullable=False))
     
-    role_id: int | None = Field(default=None, foreign_key="roles.id")
-    role : RoleEntity | None = Relationship(back_populates="users")
-    operations: list["OperationEntity"] = Relationship(back_populates="operator")
+    role_id: Optional[int] = Field(default=None, foreign_key="roles.id")
+    role : Optional["RoleEntity"] = Relationship(back_populates="users")
+    
+    operations: list["OperationEntity"] = Relationship(back_populates="operator", sa_relationship_kwargs={"lazy": "selectin"})
+    bids: list["BidEntity"] = Relationship(back_populates="investor", sa_relationship_kwargs={"lazy": "selectin"})
 
     created_by: str | None = Field(sa_column=Column(String, nullable=False))
     updated_by: str | None = Field(default=None, nullable=True)
     time_created: datetime | None = Field(default_factory=datetime.utcnow, sa_column=Column(DateTime, server_default=func.now()))
-    time_updated: Optional[datetime] = Field(default=None, nullable=True)
+    time_updated: Optional[datetime] = Field(default_factory=datetime.utcnow, sa_column=Column(DateTime, server_default=func.now()))
 
     def __str__(self):
         return {
